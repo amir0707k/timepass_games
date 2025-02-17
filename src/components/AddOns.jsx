1a5b917875fbeb6439133fc2d1a7c2c5bab09ab5
Aamir <amirk0707@gmail.com>
2025-02-17 22:32:04 +0530

    completed UI

--- src/components/AddOns.jsx
+++ src/components/AddOns.jsx
@@ -1,17 +1,7 @@
 "use client";
 import { useState } from "react";
-<<<<<<< HEAD
-<<<<<<<< HEAD:src/components/AddOns.jsx
-import { Checkbox } from "../components/ui/checkbox";
-import { Card } from "../components/ui/card";
-========
 import { Checkbox } from "@/components/ui/checkbox";
 import { Card } from "@/components/ui/card";
->>>>>>>> desktop-version:src/Components/AddOns.jsx
-=======
-import { Checkbox } from "@/components/ui/checkbox";
-import { Card } from "@/components/ui/card";
->>>>>>> desktop-version
 
 import { useSubscription } from "../context/SubscriptionContext";
 
