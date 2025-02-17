b1bf784839fa88b5532781f86eabbf89b461265b
Aamir <amirk0707@gmail.com>
2025-02-17 20:35:10 +0530

    still fixing issues

--- src/components/AddOns.jsx
+++ src/components/AddOns.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Card } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox.tsx";
+import { Card } from "@/components/ui/card.tsx";
 
 import { useSubscription } from "../context/SubscriptionContext";
 
